45d158ef54e94e92b4aa5d74cfbc0cdd71b764d9
zxTyphoon <seantylerstraub@gmail.com>
2024-02-27 03:17:59 +0100

    "Navbar + small changes"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,19 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
+		<h1 class="h1">Welcome!</h1>
 		<p>Start by exploring:</p>
 		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-			<li><a href="/furtherPage">Click here to go to a furtherPage</a></li>
+			<li><code class="code">/src/routes/+layout.svelte</code> - layout with Navbar</li>
+			<li><code class="code">/tailwind.config.js</code> - app wide css</li>
+			<li><code class="code">/src/routes/+page.svelte</code> - this page</li>
+			<li><code class="code">/src/routes/furtherPage/+page.svelte</code> - another page</li>
+			<li>
+				<a href="/furtherPage" class="text-blue-600 hover:text-blue-800 underline">
+					Click here to go to routes/furtherPage/+page.svelte
+				</a>
+			</li>							
 		</ul>
 	</div>
 </div>
--- src/routes/furtherPage/+page.svelte
+++ src/routes/furtherPage/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	let message = "Hi, I'm the further page!";
+	let message = "Hi, this is how you could embed code!";
 </script>
 
 <main>
@@ -7,7 +7,13 @@
 		<div class="space-y-5">
 			<h1>Further Page</h1>
 			<p>{message}</p>
-			<p>Made by Tyler :P</p>
+			<p>Above this is how you can use variables in the html part</p>
 		</div>
+
+	</div>
+	<div class="container h-full mx-auto flex justify-center items-center mt-4">
+		<h2>
+		<a href = "../" class="text-blue-600 hover:text-blue-800 underline">Click here to go back!</a>
+		</h2>
 	</div>
 </main>
