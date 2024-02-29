2ade7c8f85811892b914702b17afe94c217e65a9
Uiyoung Kim <s221683@student.dhbw-mannheim.de>
2024-02-29 20:19:24 +0100

    Registar page structure

---
+++ src/routes/Register/+page.svelte
@@ -0,0 +1,37 @@
+<div class="min-h-screen flex items-center justify-center bg-background-500">
+    <div class="max-w-md w-full space-y-8 p-10 bg-background-50 rounded-lg shadow-md">
+      <div>
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-primary-300">
+          Register
+        </h2>
+        <p class="mt-2 text-center text-sm text-primary-300">
+          Or <a href="javascript:history.back()" class="font-medium text-accent-400 hover:text-accent-350">
+            log in
+          </a> if you already have an account.
+        </p>
+      </div>
+      <form class="mt-8 space-y-6" action="#" method="POST">
+        <input type="hidden" name="remember" value="true">
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="email-address" class="sr-only">Email Address</label>
+            <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:z-10 sm:text-sm" placeholder="Email Address">
+          </div>
+          <div>
+            <label for="username" class="sr-only">Username</label>
+            <input id="username" name="username" type="text" autocomplete="username" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:z-10 sm:text-sm" placeholder="Username">
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:z-10 sm:text-sm" placeholder="Password">
+          </div>
+        </div>
+  
+        <div>
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-background-50 bg-primary-300 hover:bg-primary-350 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
+            Become a Member
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
