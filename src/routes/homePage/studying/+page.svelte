12f6289fb2af4da9d21279cb7829c8c816dbe1e2
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-03-07 13:48:20 +0100

    first tries

--- src/routes/homePage/studying/+page.svelte
+++ src/routes/homePage/studying/+page.svelte
@@ -1,4 +1,7 @@
-<!--- Study ---> 
+<!--- Study --->
+<script>
+	let frontsite = "Frontseite";
+</script>
 <main>
 	<div class="container h-full mx-auto flex justify-center items-center mt-4">
 		<div class="space-y-5">
@@ -7,6 +10,22 @@
 		</div>
 
 	</div>
+
+	<div class="container h-full mx-auto flex justify-center items-center mt-4">
+		<div class="bg-primary-60 rounded-lg shadow-md p-4 w-5/6 ">
+			<h2 class="text-xl font-bold mb-2 text-center text-primary-900">Vorderseite</h2>
+			<textarea class="border border-gray-300 p-2 w-full h-56 text-primary-900" bind:value={frontsite}></textarea>
+		</div>
+	</div>
+
+	<div class="container h-full mx-auto flex justify-center items-center mt-4">
+
+	<button class="bg-accent-300 hover:bg-primary-60 hover:text-text-400 text-text-50 font-bold py-2 px-4 rounded">
+		Antwort zeigen
+	</button>
+</div>
+
+
 	<div class="container h-full mx-auto flex justify-center items-center mt-4">
 		<h2>
 		<a href = "../homePage" class="text-blue-600 hover:text-blue-800 underline">Click here to go back!</a>
