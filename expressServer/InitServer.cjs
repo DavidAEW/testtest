61f65d4939a294fc0a80d704041f80f74771bea9
DavidAEW <d4v1d43w@gmail.com>
2024-03-11 13:17:04 +0100

    fix

--- expressServer/InitServer.cjs
+++ expressServer/InitServer.cjs
@@ -6,10 +6,11 @@ const cors = require('cors'); // Importiere das cors-Modul
 const argon2 = require('argon2'); // Für das Hashen von Passwörtern
 const jwt = require('jsonwebtoken'); // Für das Erstellen von JSON Web Tokens
 const cookieParser = require('cookie-parser'); // Für das Verarbeiten von Cookies
+
 require('dotenv').config(); // Lädt die .env-
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 // Aktiviere CORS für alle Anfragen
 app.use(
 	cors({
@@ -24,11 +25,11 @@ app.use(cookieParser()); //-> Cookies verarbeiten z.B. req.cookies["jwt"]
 const db = knex({
 	client: 'mysql2',
 	connection: {
-		host: 'dankikarten.mysql.database.azure.com',
-		port: 3306,
-		user: 'louis',
-		password: 'GogoH1+5',
-		database: 'ankikarten'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 	}
 });
 
