c173543e465f8e07a26b804b5c5e702690ea2c1e
zxTyphoon <seantylerstraub@gmail.com>
2024-03-24 23:21:56 +0100

    Revert "Merge pull request #18 from WWI22SEA/ui3_ui4"
    This reverts commit 8307af7c81eb2ffde66bc7bf8cf651154cf0bb72, reversing
    changes made to 8d9f6ff6cd47f5899c71aa7320bc74416faccaf1.

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -8,8 +8,8 @@
 
 <nav class="list-nav p-4">
 	<ul>
-		<li><a href="/homePage" on:click={drawerClose}>Homepage</a></li>
-		<li><a href="/navbarPages/about" on:click={drawerClose}>About</a></li>
-		<li><a href="/navbarPages/contact" on:click={drawerClose}>Contact</a></li>
+		<li><a href="../homePage" on:click={drawerClose}>Homepage</a></li>
+		<li><a href="../navbarPages/about" on:click={drawerClose}>About</a></li>
+		<li><a href="../navbarPages/contact" on:click={drawerClose}>Contact</a></li>
 	</ul>
 </nav>
