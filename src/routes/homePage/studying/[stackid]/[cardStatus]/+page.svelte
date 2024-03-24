7ab095dc988ee9b83e63d991dea3a32a72007596
zxTyphoon <seantylerstraub@gmail.com>
2024-03-24 22:57:02 +0100

    Revert "Merge branch 'ui6' into dev"
    This reverts commit 22833462ca2c382df3201784f834f2eb1cf3437a, reversing
    changes made to 08a5929e0d0f597ee4a7a9cad910e500c7108497.

--- src/routes/homePage/studying/[stackid]/[cardStatus]/+page.svelte
+++ src/routes/homePage/studying/[stackid]/[cardStatus]/+page.svelte
@@ -13,8 +13,8 @@
 		{ value: 3, label: 'kann ich' }
 	];
 	let showBack = false;
-	console.log('stackId: ' + stackId);
-	console.log('cardStatus: ' + cardStatus);
+	console.log("stackId: "+stackId);
+	console.log("cardStatus: "+cardStatus);
 	function toggleBack() {
 		showBack = !showBack;
 	}
@@ -23,6 +23,7 @@
 	}
 	let cardData = null;
 	let error = null;
+
 
 	async function getDecks() {
 		const API_URL = 'http://localhost:3001/SelectAllFromStack';
@@ -37,31 +38,31 @@
 
 	async function getCards() {
 		const API_URL = 'http://localhost:3001/GetRandomCardWithStatus';
-		try {
-			const response = await fetch(API_URL, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					cardStatus: Number(cardStatus),
-					stackId: Number(stackId)
-				})
-			});
-			const data = await response.json();
-			console.log(data);
+try {
+	const response = await fetch(API_URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			cardStatus: Number(cardStatus),
+			stackId: Number(stackId)
+		})
+	});
+	const data = await response.json();
+	console.log(data);
 
-			if (response.ok) {
-				cardData = data;
-			} else {
-				error = data.error;
-			}
-		} catch (error) {
-			console.error('Fehler beim Laden der Karten:', error);
-			cardData = null;
-		}
+	if (response.ok) {
+		cardData = data;
+	} else {
+		error = data.error;
 	}
-	async function updateCardStatus(cardId, status) {
+}catch(error) {
+	console.error('Fehler beim Laden der Karten:', error);
+	cardData = null;
+}
+	}
+	async function updateCardStatus(cardId) {
 		const API_URL = `http://localhost:3001/UpdateCardStatusTo1`;
 
 		const response = await fetch(API_URL, {
@@ -72,26 +73,83 @@
 			body: JSON.stringify({
 				front: cardData.front,
 				back: cardData.back,
-				newCardStatus: status 
+				newCardStatus: 1 // Set status to 1 (learned)
 			})
 		});
 
 		if (!response.ok) {
+			//window.location.reload();
 			console.error('Error updating card status:', await response.text());
 			return;
 		}
 
 		console.log('Card status updated successfully!');
+		// You can optionally fetch a new card here
+		//window.location.reload();
+		await getCards();
+	}
+
+	async function updateCardStatusTo2(cardId) {
+		const API_URL = `http://localhost:3001/UpdateCardStatusTo2`; // Update URL with your endpoint
+
+		const response = await fetch(API_URL, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				front: cardData.front,
+				back: cardData.back,
+				newCardStatus: 2 // Set status to 1 (learned)
+			})
+		});
+
+		if (!response.ok) {
+			//window.location.reload();
+			console.error('Error updating card status:', await response.text());
+			return;
+		}
+
+		console.log('Card status updated successfully!');
+		// You can optionally fetch a new card here
+		//window.location.reload();
+		await getCards();
+	}
+
+	async function updateCardStatusTo3(cardId) {
+		const API_URL = `http://localhost:3001/UpdateCardStatusTo3`; // Update URL with your endpoint
+
+		const response = await fetch(API_URL, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				front: cardData.front,
+				back: cardData.back,
+				newCardStatus: 3 // Set status to 1 (learned)
+			})
+		});
+
+		if (!response.ok) {
+			//window.location.reload();
+			console.error('Error updating card status:', await response.text());
+			return;
+		}
+
+		console.log('Card status updated successfully!');
+		// You can optionally fetch a new card here
+		//window.location.reload();
 		await getCards();
 	}
 
 	async function handleChangeStatus(event) {
-		goto('/homePage/studying/' + stackId + '/' + event.target.value + '/');
+		goto('/homePage/studying/'+stackId+'/'+event.target.value+'/');
 		console.log(cardStatus);
 		await getCards();
 	}
 	async function handleChangeID(event) {
-		goto('/homePage/studying/' + event.target.value + '/' + cardStatus + '/');
+		goto('/homePage/studying/'+event.target.value+'/'+cardStatus+'/');
 		console.log(cardStatus);
 		await getCards();
 	}
@@ -103,15 +161,13 @@
 </script>
 
 <main>
+
 	<div class="container h-full mx-auto flex justify-center items-center mt-4">
 		<div class="bg-primary-60 dark:bg-secondary-250 rounded-lg shadow-md p-4 w-5/6 flex-row flex">
+
 			<h2 class="font-bold mb-2 text-center text-primary-900 my-auto mr-2">Stapel</h2>
 
-			<select
-				class="border overflow-wrap: break-words border-gray-300 p-2 w-full h-auto rounded text-primary-900 dark:text-primary-400 bg-background-0 dark:bg-primary-60 mr-4"
-				bind:value={stackId}
-				on:change={handleChangeID}
-			>
+			<select class="border overflow-wrap: break-words border-gray-300 p-2 w-full h-auto rounded text-primary-900 dark:text-primary-400 bg-background-0 dark:bg-primary-60 mr-4" bind:value={stackId} on:change={handleChangeID}>
 				{#each options as option}
 					<option value={option.value}>
 						{option.label}
@@ -119,13 +175,10 @@
 				{/each}
 			</select>
 
-			<h2 class="font-bold mb-2 text-center text-primary-900 my-auto mr-2">Status</h2>
 
-			<select
-				class="border overflow-wrap: break-words border-gray-300 p-2 w-full h-auto rounded text-primary-900 dark:text-primary-400 bg-background-0 dark:bg-primary-60"
-				bind:value={cardStatus}
-				on:change={handleChangeStatus}
-			>
+		<h2 class="font-bold mb-2 text-center text-primary-900 my-auto mr-2">Status</h2>
+
+		<select class="border overflow-wrap: break-words border-gray-300 p-2 w-full h-auto rounded text-primary-900 dark:text-primary-400 bg-background-0 dark:bg-primary-60" bind:value={cardStatus} on:change={handleChangeStatus}>
 				{#each cardOptions as option}
 					<option value={option.value}>
 						{option.label}
@@ -144,33 +197,37 @@
 				<div
 					class="border overflow-wrap: break-words border-gray-300 p-2 w-full h-auto rounded text-primary-900 dark:text-primary-400 bg-background-0 dark:bg-primary-60"
 				>
-					Du hast alle Karten mit diesem Status/in diesem Deck gelernt! Füge neue Karten hinzu oder
-					lerne alte Karten erneut, um diese weiter zu festigen.
+					Du hast alle Karten mit diesem Status/in diesem Deck  gelernt! Füge neue Karten hinzu oder lerne alte Karten erneut,
+					um diese weiter zu festigen.
 				</div>
 			</div>
 		</div>
 	{/if}
 
 	{#if cardData}
-		<div class="container h-full mx-auto flex justify-center items-center mt-4">
-			<div class="bg-primary-60 dark:bg-secondary-250 rounded-lg shadow-md p-4 w-5/6">
-				<h2 class="text-xl font-bold mb-2 text-center text-primary-900">Vorderseite</h2>
-				<div
-					class="border overflow-wrap: break-words border-gray-300 p-2 w-full h-auto rounded text-primary-900 dark:text-primary-400 bg-background-0 dark:bg-primary-60"
-				>
-					{cardData?.front}
-				</div>
+	<div class="container h-full mx-auto flex justify-center items-center mt-4">
+		<div class="bg-primary-60 dark:bg-secondary-250 rounded-lg shadow-md p-4 w-5/6">
+			<h2 class="text-xl font-bold mb-2 text-center text-primary-900">Vorderseite</h2>
+			<div
+				class="border overflow-wrap: break-words border-gray-300 p-2 w-full h-auto rounded text-primary-900 dark:text-primary-400 bg-background-0 dark:bg-primary-60"
+			>
+				{cardData?.front}
 			</div>
 		</div>
+	</div>
 
-		<div class="container h-full mx-auto flex justify-center items-center mt-4">
-			<button
-				class="bg-primary-60 dark:bg-accent-300 dark:hover:bg-primary-60 dark:hover:text-text-400 hover:bg-accent-300 hover:text-text-50 text-primary-400 dark:text-text-50 font-bold py-2 px-4 rounded"
-				on:click={toggleBack}
-			>
-				{showBack ? 'Antwort ausblenden' : 'Antwort zeigen'}
-			</button>
-		</div>
+
+
+
+
+	<div class="container h-full mx-auto flex justify-center items-center mt-4">
+		<button
+			class="bg-primary-60 dark:bg-accent-300 dark:hover:bg-primary-60 dark:hover:text-text-400 hover:bg-accent-300 hover:text-text-50 text-primary-400 dark:text-text-50 font-bold py-2 px-4 rounded"
+			on:click={toggleBack}
+		>
+			{showBack ? 'Antwort ausblenden' : 'Antwort zeigen'}
+		</button>
+	</div>
 	{/if}
 
 	{#if showBack}
@@ -187,19 +244,19 @@
 		<div class="container h-full mx-auto flex justify-center items-center mt-4">
 			<button
 				class="bg-primary-60 dark:bg-accent-300 dark:hover:bg-primary-60 dark:hover:text-text-400 hover:bg-accent-300 hover:text-text-50 text-primary-400 dark:text-text-50 font-bold py-2 px-4 rounded mr-16"
-				on:click={() => updateCardStatus(cardData.id, 1)}
+				on:click={() => updateCardStatus(cardData.id)}
 			>
 				gelernt
 			</button>
 			<button
 				class="bg-primary-60 dark:bg-accent-300 dark:hover:bg-primary-60 dark:hover:text-text-400 hover:bg-accent-300 hover:text-text-50 text-primary-400 dark:text-text-50 font-bold py-2 px-4 rounded mr-16"
-				on:click={() => updateCardStatus(cardData.id, 2)}
+				on:click={() => updateCardStatusTo2(cardData.id)}
 			>
 				kann ich fast
 			</button>
 			<button
 				class="bg-primary-60 dark:bg-accent-300 dark:hover:bg-primary-60 dark:hover:text-text-400 hover:bg-accent-300 hover:text-text-50 text-primary-400 dark:text-text-50 font-bold py-2 px-4 rounded"
-				on:click={() => updateCardStatus(cardData.id, 3)}
+				on:click={() => updateCardStatusTo3(cardData.id)}
 			>
 				weiß ich
 			</button>
