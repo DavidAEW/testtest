08a5929e0d0f597ee4a7a9cad910e500c7108497
zxTyphoon <seantylerstraub@gmail.com>
2024-03-24 22:45:52 +0100

    Revert "Merge branch 'ui6' into dev"
    This reverts commit c22b550c8f87713e470eae2ca866645fb247e139, reversing
    changes made to 92639a19586f25acd715a287f8ecf69b217815d8.

--- src/routes/homePage/+page.svelte
+++ src/routes/homePage/+page.svelte
@@ -1,110 +1,59 @@
-<script>
-	import { onMount } from 'svelte';
-
-	let options = [];
-
-	async function getOptions() {
-		const API_URL = 'http://localhost:3001/SelectAllFromStack';
-		const response = await fetch(API_URL);
-		const data = await response.json();
-
-		options = data.map((item) => ({
-			value: item.stackid,
-			label: item.stackname
-		}));
-	}
-	async function getUserInfo() {
-		const url = 'http://localhost:3001/user/';
-		try {
-			const response = await fetch(url, {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				credentials: 'include'
-			});
-
-			if (response.ok) {
-				const userData = await response.json();
-				document.getElementById('usernameDisplay').textContent =
-					`Welcome back ${userData.username}`;
-			} else {
-				console.error('Fehler beim Abrufen der Benutzerdaten.');
-			}
-		} catch (error) {
-			console.error('Fehler:', error);
-		}
-	}
-
-	let selectedOption = '';
-
-	function handleChange(event) {
-		selectedOption = event.target.value;
-		console.log(selectedOption);
-	}
-
-	onMount(() => {
-		getUserInfo();
-		getOptions();
-	});
-</script>
-
-<main class="h-full overflow-hidden">
-	<div class="flex justify-center items-center">
-		<h1 class="text-3xl font-extrabold text-text-100 mt-4" id="usernameDisplay">Welcome back</h1>
-	</div>
-	<div class="h-full flex justify-center items-center space-y-5">
-		<div class="space-y-5">
-			<h2 class="text-2xl font-bold my-5">Choose a deck to study</h2>
-			<div class="space-y-5">
-				<select
-					class="dark:bg-primary-60 bg-primary-0 border border-gray-200 text-primary-400 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-					on:change={handleChange}
+<!--- Homepage --->
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Homepage</h1>
+		<p>These are some of the other pages:</p>
+		<ul>
+			<li>
+				<code class="text-text-100 bg-secondary-60 p-1 rounded font-mono text-xs font-normal">
+					/src/routes/+layout.svelte
+				</code> - layout with Navbar
+			</li>
+			<li>
+				<code class="text-text-100 bg-secondary-60 p-1 rounded font-mono text-xs font-normal">
+					/tailwind.config.js
+				</code> - app wide css
+			</li>
+			<li>
+				<code class="text-text-100 bg-secondary-60 p-1 rounded font-mono text-xs font-normal">
+					/src/routes/homePage/+page.svelte
+				</code> - this page
+			</li>
+			<li>
+				<code class="text-text-100 bg-secondary-60 p-1 rounded font-mono text-xs font-normal">
+					/src/routes/homePage/addKarteikarten/+page.svelte
+				</code> - add Karteikarten
+			</li>
+			<li>
+				<code class="text-text-100 bg-secondary-60 p-1 rounded font-mono text-xs font-normal">
+					/src/routes/homePage/manageKarteikarten/+page.svelte
+				</code> - manage Karteikarten
+			</li>		
+			<li>Click here to go to
+				<a href="/homePage/addKarteikarten" class="text-blue-600 hover:text-blue-800 underline"
+					>routes/addKarteikarten/+page.svelte</a
 				>
-					<option value="">Deck auswählen</option>
-					{#each options as option}
-						<option value={option.value}>{option.label}</option>
-					{/each}
-				</select>
-				<a href="/homePage/studying/{selectedOption}" class="dark:bg-primary-200 bg-primary-0 border border-gray-200 text-primary-400 py-3 px-4 rounded-full focus:bg-white focus:border-gray-500">Start Studying</a>
-			</div>
-
-			<div class="space-y-5 mt-10">
-				<p>These are some of the other pages:</p>
-				<ul class="space-y-1">
-					<li>
-						Click here to go to
-						<a href="/homePage/addKarteikarten" class="text-blue-600 hover:text-blue-800 underline"
-							>routes/addKarteikarten/+page.svelte</a
-						>
-					</li>
-					<li>
-						Click here to go to
-						<a
-							href="/homePage/manageKarteikarten"
-							class="text-blue-600 hover:text-blue-800 underline"
-							>routes/manageKarteikarten/+page.svelte</a
-						>
-					</li>
-					<li>
-						Click here to go to
-						<a href="/homePage/manageDecks" class="text-blue-600 hover:text-blue-800 underline"
-							>routes/manageDecks/+page.svelte</a
-						>
-					</li>
-					<li>
-						Click here to go to
-						<a href="/homePage/studying" class="text-blue-600 hover:text-blue-800 underline"
-							>routes/studying/+page.svelte</a
-						>
-					</li>
-					<li>
-						<a href="/farben" class="text-blue-600 hover:text-blue-800 underline"
-							>Click here to go checkout our colorscheme</a
-						>
-					</li>
-				</ul>
-			</div>
-		</div>
+			</li>
+			<li>Click here to go to
+				<a href="/homePage/manageKarteikarten" class="text-blue-600 hover:text-blue-800 underline"
+					>routes/manageKarteikarten/+page.svelte</a
+				>
+			</li>
+			<li>Click here to go to
+				<a href="/homePage/manageDecks" class="text-blue-600 hover:text-blue-800 underline"
+				>routes/manageDecks/+page.svelte</a
+				>
+			</li>
+			<li>Click here to go to
+				<a href="/homePage/studying/2" class="text-blue-600 hover:text-blue-800 underline"
+					>routes/studying/+page.svelte</a
+				>
+			</li>
+			<li>
+				<a href="/farben" class="text-blue-600 hover:text-blue-800 underline"
+					>Click here to go checkout our colorscheme</a
+				>
+			</li>
+		</ul>
 	</div>
-</main>
+</div>
