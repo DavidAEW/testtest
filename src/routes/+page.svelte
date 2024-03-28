360212b746b5d2a292b21791c74bcee5f5672a62
zxTyphoon <seantylerstraub@gmail.com>
2024-03-28 18:19:02 +0100

    Landing page redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,34 @@
 <script>
   import { goto } from '$app/navigation';
-  
+	import { onMount } from 'svelte';
+
+  let loggedIn = false;
+	async function getUserInfo() {
+		const url = 'http://localhost:3001/user/';
+		try {
+			const response = await fetch(url, {
+				method: 'GET',
+        credentials: 'include',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				credentials: 'include'
+			});
+
+			if (response.ok) {
+				const userData = await response.json();
+				loggedIn = true;
+			} else {
+				console.error('Fehler beim Abrufen der Benutzerdaten.');
+			}
+		} catch (error) {
+			console.error('Fehler:', error);
+		}
+	}
+
+  onMount(() => {
+     getUserInfo();
+  });
 
   let email = '';
   let password = '';
@@ -42,30 +70,39 @@
       console.error('Error: ', error);
     }
   }
+
+  function handleRedirect() {
+    goto("/homePage");
+  }
 </script>
-<main class="h-full">
-  <div class="grid h-full justify-center items-center ">
-    <div class="w-full  max-w-md space-y-8 bg-background-70 dark:bg-background-90 shadow-md rounded-lg px-6 py-8">
-      <h1 class="text-2xl sm:text-3xl font-bold text-center text-primary-300 mb-6">Log in</h1>
-      <form class="space-y-6" on:submit={handleSubmit}>
-        <div>
-          <label for="email" class="block text-sm font-bold mb-2 text-text-300">Email</label>
-          <input type="email" id="email" bind:value={email} required class="appearance-none border rounded w-full py-2 px-3 text-text-300 leading-tight focus:outline-none focus:shadow-outline" />
+
+{#if loggedIn}
+  {handleRedirect()}
+  {:else}
+  <main class="h-full">
+    <div class="grid h-full justify-center items-center ">
+      <div class="w-full  max-w-md space-y-8 bg-background-70 dark:bg-background-90 shadow-md rounded-lg px-6 py-8">
+        <h1 class="text-2xl sm:text-3xl font-bold text-center text-primary-300 mb-6">Log in</h1>
+        <form class="space-y-6" on:submit={handleSubmit}>
+          <div>
+            <label for="email" class="block text-sm font-bold mb-2 text-text-300">Email</label>
+            <input type="email" id="email" bind:value={email} required class="appearance-none border rounded w-full py-2 px-3 text-text-300 leading-tight focus:outline-none focus:shadow-outline" />
+          </div>
+          <div>
+            <label for="password" class="block text-sm font-bold mb-2 text-text-300">Password</label>
+            <input type="password" id="password" bind:value={password} required class="appearance-none border rounded w-full py-2 px-3 text-text-300 leading-tight focus:outline-none focus:shadow-outline" />
+          </div>
+          <button type="submit" class="w-full bg-primary-250 hover:bg-primary-100 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Log in
+          </button>
+        </form>
+        
+        <div class="text-center mt-8">
+          <a href="/Register" class="px-6 py-2 text-sm font-semibold bg-primary-100 hover:bg-primary-90 dark:bg-primary-200 rounded-full dark:hover:bg-primary-100 focus:outline-none focus:ring-2 focus:ring-primary-300 focus:ring-opacity-50">
+            Register
+          </a>
         </div>
-        <div>
-          <label for="password" class="block text-sm font-bold mb-2 text-text-300">Password</label>
-          <input type="password" id="password" bind:value={password} required class="appearance-none border rounded w-full py-2 px-3 text-text-300 leading-tight focus:outline-none focus:shadow-outline" />
-        </div>
-        <button type="submit" class="w-full bg-primary-250 hover:bg-primary-100 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          Log in
-        </button>
-      </form>
-     
-      <div class="text-center mt-8">
-        <a href="/Register" class="px-6 py-2 text-sm font-semibold bg-primary-100 hover:bg-primary-90 dark:bg-primary-200 rounded-full dark:hover:bg-primary-100 focus:outline-none focus:ring-2 focus:ring-primary-300 focus:ring-opacity-50">
-          Register
-        </a>
       </div>
     </div>
-  </div>
-</main>
+  </main>
+  {/if}
